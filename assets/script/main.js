77fc684e034a5847e88f0c9aa480f654983a42b4
kurobusaram <fhksramd4-mbdp@outlook.com>
2024-08-11 10:15:45 +0900

    20240810_yoshida

--- assets/script/main.js
+++ assets/script/main.js
@@ -17,3 +17,29 @@ $(window).on('resize', function () {
   //ハンバーガーメニュー閉じる
   hamburger.removeClass('header__nav--hamburger-active');
 });
+
+//faq
+//アコーディオン
+$('.faq__contents--q').on('click', function (e) {
+  var content = $(this).next();
+  content.slideToggle(300);
+  $('.q__headding--btnP', this).toggleClass("open", 300);
+});
+
+//price
+//スライダー
+var splide = new Splide( '.splide', {
+  type   : 'loop',
+  perPage: 1,
+  gap   : '1.5rem',
+
+
+  focus: 'center',
+  arrows: false,
+  classes: {
+    pagination: "splide__pagination gallery__pagination",
+    page: "splide__pagination__page gallery__pagination--page",
+  },
+} );
+splide.mount();
+
